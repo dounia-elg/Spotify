3f0a000283002318257433fe9add66c058518032
dounia-elg <dounia.elg20@gmail.com>
2025-02-07 15:40:30 +0100

    added header in home page

--- views/home.php
+++ views/home.php
@@ -7,12 +7,13 @@
     <title>Home</title>
 </head>
 <body class="bg-gray-100 text-gray-900">
+    <?php include 'header.php'; ?>
     <div class="container mx-auto p-6 text-center">
         <h1 class="text-3xl font-bold flex items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-2v13M9 10l12-2" />
             </svg>
-            Welcome to Spotify
+            Welcome to Music Platform
         </h1>
         <p class="mt-4 text-gray-700">Discover and enjoy your favorite music.</p>
     </div>
--- views/register.php
+++ views/register.php
@@ -20,11 +20,10 @@
             <input class="w-full p-2 border rounded mt-2" type="password" name="password" placeholder="Password" required>
             <button class="w-full bg-green-500 text-white p-2 rounded mt-4 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
                 Register
             </button>
         </form>
     </div>
 </body>
-</html>
