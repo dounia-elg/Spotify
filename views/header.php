13e96dfdc77cffbc38468b5c66ad75ef301ad600
dounia-elg <dounia.elg20@gmail.com>
2025-02-07 15:43:26 +0100

    Created a header

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<header class="bg-gray-800 text-white p-4 flex justify-between items-center">
+    <h1 class="text-xl font-bold">Music Platform</h1>
+    <nav>
+        <a href="home.php" class="px-3">Home</a>
+        <a href="playlists.php" class="px-3">Playlists</a>
+        <a href="songs.php" class="px-3">Songs</a>
+        <a href="login.php" class="px-3">Login</a>
+        <a href="register.php" class="px-3">Sign Up</a>
+    </nav>
+</header>
