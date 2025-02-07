3fc6a259fef45b84f17397d1f03485940c70dfda
dounia-elg <dounia.elg20@gmail.com>
2025-02-07 12:06:15 +0100

    Created RegistredUser classe

--- models/RegisteredUser.php
+++ models/RegisteredUser.php
@@ -2,27 +2,9 @@
 require_once 'AbstractUser.php';
 
 class RegisteredUser extends AbstractUser {
-    public function __construct($id, $name, $email, $password) {
-        parent::__construct($id, $name, $email, $password, 'user');
-    }
-
-    public function register($name, $email, $password, $pdo) {
-        try {
-           
-            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-            $stmt = $pdo->prepare("INSERT INTO users (name, email, password_hash, role) VALUES (:name, :email, :password, 'user')");
-            
-            $stmt->execute([
-                'name' => $name,
-                'email' => $email,
-                'password' => $hashedPassword
-            ]);
-
-            return "User registered successfully!";
-        } catch (PDOException $e) {
-            return "Error registering user: " . $e->getMessage();
-        }
+    public function register($db) {
+        $hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
+        $stmt = $db->prepare("INSERT INTO users (name, email, password_hash, role) VALUES (?, ?, ?, 'user')");
+        return $stmt->execute([$this->name, $this->email, $hashedPassword]);
     }
 }
-?>
